de0e6852998a62f601e1cc8b7e7bf295088b011b
Rajesh <charanrajesh162@gmail.com>
2023-11-26 14:09:19 +0530

    insure-me project has been updated

---
+++ src/main/java/Project/insureme/PolicyService.java
@@ -0,0 +1,53 @@
+package Project.insureme;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PolicyService {
+	
+	@Autowired
+	PolicyRepository policyRepository;
+	
+	
+	public Policy CreatePolicy() {
+		
+		Policy policy = generateDummyPolicy();
+		
+		return policyRepository.save(policy);
+		
+			
+	}
+	
+	public Policy updatePolicy() {
+		
+		return null;
+	}
+	
+	public Policy deletePolicy() {
+		
+		return null;
+	}
+	
+	public Policy searchPolicy() {
+		
+		return null;
+	}
+	
+	
+	public Policy generateDummyPolicy() {
+		return new Policy(1, "Rajesh", "Individual" , 20000, "01-Jan-2021", "01-Jan-2022");
+	}
+
+	public Policy registerPolicy(Policy policy) {
+		// TODO Auto-generated method stub
+		return policyRepository.save(policy);
+	}
+
+	public Policy getPolicyDetails(int policyId) {
+		// TODO Auto-generated method stub
+		return policyRepository.findById(policyId).get();
+	}
+
+}
---
+++ src/test/java/Project/insureme/InsureMeApplicationTests.java
@@ -0,0 +1,31 @@
+package Project.insureme;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsureMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void testCreatePolicy() {
+		Policy policy = new Policy(1, "Rajesh", "Individual" , 20000, "01-Jan-2021", "01-Jan-2022");
+		PolicyService pService = new PolicyService();
+		//Policy outputPolicy = pService.CreatePolicy();
+		assertEquals(policy.getPolicyId(), pService.generateDummyPolicy().getPolicyId());
+		
+	}
+	
+	@Test
+	void testSearchPolicy() {
+		PolicyService pService = new PolicyService();
+		assertEquals(null,pService.searchPolicy());
+	}
+
+	
+}
